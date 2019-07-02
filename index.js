32cd42ae49fefbd43b3c655eb7bd6a77d57994c1
Meredith Murphy <meredithcat@gmail.com>
2019-07-02 09:58:39 -0700

    this is a commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Assign a new method to String.prototype
-String.prototype.allCaps = function() {
-  return this.toUpperCase()
-}
+String.prototype.allCaps = function () {
+    return this.toUpperCase();
+};
